3e29897109a404f128a9fc56c0912f1fd15a6824
bytemasterhassan <hassanshahid340@gmail.com>
2024-11-26 15:12:41 +0500

    new commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, GCP from Docker and Jenkins!. From Jenkins Automation pipeline (finally)"
+    return "Hello, GCP from Docker and Jenkins!.From Jenkins Automation pipeline (finally)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
