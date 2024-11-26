ec4f2503fc8ee09d28d1155b7554c72bd2da4f1b
bytemasterhassan <hassanshahid340@gmail.com>
2024-11-26 14:55:09 +0500

    new commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, GCP from Docker and Jenkins!"
+    return "Hello, GCP from Docker and Jenkins!. From Jenkins Automation pipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
---
+++ git.sh
@@ -0,0 +1,4 @@
+#sh git.sh
+git add .
+git commit -m "new commit"
+git push origin main
